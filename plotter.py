5e80945b394976ace5dc8dc6d4ae68f3fff19e7c
Anand Jeyahar <anand.jeyahar@gmail.com>
2016-06-17 22:26:00 +0530

    atleast one class roc_plot works. .. but still

--- plotter.py
+++ plotter.py
@@ -393,34 +393,92 @@ def sb_jointplot(series1, series2):
     # Show the joint distribution using kernel density estimation
     return sns.jointplot(series1, series2, kind="kde", size=7, space=0)
 
-def roc_plot(classifier, dataframe, target, multi_class=True):
+def cross_validate():
+	for i, (train, test) in enumerate(cv):
+		score = classifier.fit(dataframe[train], target[train]).decision_function(dataframe[test])
+		# Compute ROC curve and area the curve
+		fpr, tpr, thresholds = roc_curve(target[test], probas_[:, 1])
+		mean_tpr += interp(mean_fpr, fpr, tpr)
+		mean_tpr[0] = 0.0
+		roc_auc = auc(fpr, tpr)
+		plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+
+def roc_plot(dataframe, target, score, cls_list=[],multi_class=True):
     import numpy as np
     import pandas as pd
     import matplotlib.pyplot as plt
     from sklearn.cross_validation import StratifiedKFold
-
-    assert isinstance(target, (numpy.ndarray, pd.Series))
+    from sklearn.metrics import roc_curve, auc
+    from sklearn.preprocessing import label_binarize
+    assert isinstance(target, (np.ndarray, pd.Series))
     # Not sure what this means some sort of initialization but are these right numbers?
     mean_tpr = 0.0
     mean_fpr = np.linspace(0, 1, 100)
     all_tpr = []
 
+    num_classes = target.shape[1] or 1
+    target = label_binarize(target, classes=cls_list)
     if not multi_class:
-        assert target.shape[1] == 1, "Please pass a nx1 array"
-        assert target.nunique() == 1, "Please pass a nx1 array"
+        #assert target.shape[1] == 1, "Please pass a nx1 array"
+        #assert target.nunique() == 1, "Please pass a nx1 array"
+        # Compute ROC curve and ROC area for each class
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        for i in range(num_classes):
+            fpr[i], tpr[i], _ = roc_curve(target[:, i], score[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        # Compute micro-average ROC curve and ROC area
+        fpr["micro"], tpr["micro"], _ = roc_curve(target.ravel(), score.ravel())
+        roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+        # Plot of a ROC curve for a specific class
+        plt.figure()
+        plt.plot(fpr[2], tpr[2], label='ROC curve (area = %0.2f)' % roc_auc[2])
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic example')
+        plt.legend(loc="lower right")
+        return plt
     else:
-        num_classes = target.shape[1]
-        target = label_binarize(target, num_classes)
-    cv = StratifiedKFold(target, n_folds=6)
-    for i, (train, test) in enumerate(cv):
-        print(i, train, test)
-        probas_ = classifier.fit(dataframe[train], target[train]).predict_proba(dataframe[test])
-        # Compute ROC curve and area the curve
-        fpr, tpr, thresholds = roc_curve(target[test], probas_[:, 1])
-        mean_tpr += interp(mean_fpr, fpr, tpr)
-        mean_tpr[0] = 0.0
-        roc_auc = auc(fpr, tpr)
-        plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+        # First aggregate all false positive rates
+        all_fpr = np.unique(np.concatenate([fpr[i] for i in range(num_classes)]))
+
+        # Then interpolate all ROC curves at this points
+        mean_tpr = np.zeros_like(all_fpr)
+        for i in range(num_classes):
+            mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+        # Finally average it and compute AUC
+        mean_tpr /= num_classes
+        fpr["macro"] = all_fpr
+        tpr["macro"] = mean_tpr
+        roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+        # Plot all ROC curves
+        plt.figure()
+        plt.plot(fpr["micro"], tpr["micro"],
+                 label='micro-average ROC curve (area = {0:0.2f})'
+                 ''.format(roc_auc["micro"]),
+                 linewidth=2)
+        plt.plot(fpr["macro"], tpr["macro"],
+                 label='macro-average ROC curve (area = {0:0.2f})'
+                 ''.format(roc_auc["macro"]),
+                 linewidth=2)
+
+        for i in range(num_classes):
+            plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:0.2f})'
+                     ''.format(i, roc_auc[i]))
+
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Some extension of Receiver operating characteristic to multi-class')
+        plt.legend(loc="lower right")
+        plt.show()
     #from sklearn.metrics import roc_curve
     #fpr, tpr, thresholds = roc_curve(target, scores, pos_label=2)
     plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Luck')
