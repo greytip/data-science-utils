c54eb92a619ff82deb9a8acfef9aefecf5a28a5c
Anand Jeyahar <anand.jeyahar@gmail.com>
2016-06-23 19:33:04 +0530

    more dependencies and more scikits-image filter/functions

--- skimageUtils.py
+++ skimageUtils.py
@@ -1,5 +1,6 @@
 from skimage.color.adapt_rgb import adapt_rgb, each_channel, hsv_value
 from skimage import filters
+from skimage.color import rgb2gray
 
 @adapt_rgb(each_channel)
 def sobel_each(image):
@@ -9,4 +10,10 @@ def sobel_each(image):
 def sobel_hsv(image):
     return filters.sobel(image)
 
+@adapt_rgb(as_gray)
+def sobel_gray(image):
+    return filters.sobel(image)
 
+def image_as_gray(image_filter, image, *args, **kwargs):
+    gray_image = rgb2gray(image)
+    return image_filter(gray_image, *args, **kwargs)
