7563cb70033c21e9af534486e0f912016d4f30ff
Anand Jeyahar <anand.jeyahar@gmail.com>
2016-06-23 14:35:44 +0530

    another wrapper around scikits-image library..

---
+++ skimageUtils.py
@@ -0,0 +1,13 @@
+from skimage.color.adapt_rgb import adapt_rgb, each_channel, hsv_value
+from skimage import filters
+
+
+@adapt_rgb(each_channel)
+def sobel_each(image):
+    return filters.sobel(image)
+
+
+@adapt_rgb(hsv_value)
+def sobel_hsv(image):
+    return filters.sobel(image)
+
