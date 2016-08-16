3f3a7070e6cde66774a3ea130caa1ec739148fd3
Anand Jeyahar <anand.jeyahar@gmail.com>
2016-08-16 07:45:30 +0530

    more stuff..

---
+++ analyze.py
@@ -0,0 +1,21 @@
+def correlation_analyze(df):
+    #TODO: Plot scatter plot of all combinations of column variables in the df
+    pass
+
+def regression_analyze(df):
+    # this is the quantitative/hard version of teh above
+    #TODO: plot line plot of combinatons of column variables, but for the y-axis,
+    # Fit on a, linear function(aka line)
+    #         b, logarithmic/exponetial function
+    #         c, logistic function
+    #         d, parabolic function??
+    #   Additionally plot the fitted y and the correct y in different colours against the same x
+    pass
+
+def time_series_analysis(df):
+    # TODO;
+    # 1. Do, ADF(Dickey-Fuller's ) stationarity test
+    # 2. Seasonal decomposition of the time series and plot it
+    # 3. ARIMA model of the times
+    # 4. And other time-serie models like AR, etc..
+    pass
