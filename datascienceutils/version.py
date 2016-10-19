b037cfec4315827f192ec4d612f1b74f2640a47c
anand <anand.jeyahar@gmail.com>
2016-10-19 12:58:54 +0530

    minor changes...

--- datascienceutils/version.py
+++ datascienceutils/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM SCIPY SETUP.PY
-short_version = '1.0.3'
-version = '1.0.3'
-full_version = '1.0.3'
-git_revision = '1d2a634f5d59aa88c5460a01dd49a4e39d99e809'
+short_version = '1.0.4'
+version = '1.0.4'
+full_version = '1.0.4'
+git_revision = '645e4ca171356ba1b5246485b393160813310b55'
 release = True
 
 if not release:
