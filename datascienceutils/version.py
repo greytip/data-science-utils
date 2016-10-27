d56a2af11d0e46325ae49b0f7d7f99f12fd8f16b
anand <anand.jeyahar@gmail.com>
2016-10-27 16:12:10 +0530

    version bump..

--- datascienceutils/version.py
+++ datascienceutils/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM SCIPY SETUP.PY
-short_version = '1.0.21'
-version = '1.0.21'
-full_version = '1.0.21'
-git_revision = '92db04f8ffc17a6267ff2153b7317dc69530a798'
+short_version = '1.0.22'
+version = '1.0.22'
+full_version = '1.0.22'
+git_revision = 'd4b3bcd8ca46780e6d57ae08c7db8eee0c21abc9'
 release = True
 
 if not release:
