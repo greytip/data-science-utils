30c1a7b959a83543724fc1bd88673cdc088fef9a
anand <anand.jeyahar@gmail.com>
2016-10-29 20:37:06 +0530

    Few more cleanups

--- DataExplorerTemplate.py
+++ DataExplorerTemplate.py
@@ -45,12 +45,12 @@ def createClasses(x):
 df['Class'] = df['Species'].apply(createClasses)
 
 
-# In[5]:
+# In[4]:
 
 df.describe()
 
 
-# In[6]:
+# In[5]:
 
 df.head()
 
@@ -67,13 +67,13 @@ df['Species'].unique()
 
 # In[9]:
 
-analyze.correlation_analyze(df, exclude_columns='Id', categories=['Species', 'Class'], measure=['SepalLengthCm', 
-                                                                                      'SepalWidthCm',
-                                                                                       'PetalLengthCm',
-                                                                                       'PetalWidthCm'])
+analyze.correlation_analyze(df, exclude_columns='Id', 
+                                categories=['Species', 'Class'], 
+                                measure=['SepalLengthCm','SepalWidthCm',
+                                           'PetalLengthCm', 'PetalWidthCm'])
 
 
-# In[10]:
+# In[5]:
 
 target = df.Species
 df.drop(['Species', 'Class'], 1, inplace=True)
@@ -84,12 +84,32 @@ df.drop(['Species', 'Class'], 1, inplace=True)
 #analyze.time_series_analysis(df, timeCol='date', valueCol='count')
 
 
-# In[ ]:
+# In[8]:
 
 analyze.cluster_analyze(df, cluster_type='dbscan')
 
 
 # In[ ]:
 
-analyze.som_analyze(df, (30,30), algo_type='som')
+analyze.som_analyze(df, (10,10), algo_type='som')
+
+
+# In[7]:
+
+import sompy
+som_factory = sompy.SOMFactory()
+data = df.as_matrix()
+mapsize = (10,10)
+assert isinstance(mapsize, tuple), "Mapsize must be a tuple"
+sm = som_factory.build(data, mapsize= mapsize, normalization='var', initialization='pca')
+sm.train(n_job=6, shared_memory='no', verbose='INFO')
+
+# View map
+v = sompy.mapview.View2DPacked(50, 50, 'test',text_size=8)
+v.show(sm, what='codebook', cmap='jet', col_sz=6) #which_dim=[0,1]
+v.show(sm, what='cluster', cmap='jet', col_sz=6) #which_dim=[0,1] defaults to 'all',
+
+# Hitmap
+h = sompy.hitmap.HitMapView(10, 10, 'hitmap', text_size=8, show_text=True)
+h.show(sm)
 
