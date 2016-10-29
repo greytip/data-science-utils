e8586651a5638310a945adb99ffd196e80702662
anand <anand.jeyahar@gmail.com>
2016-10-29 19:29:06 +0530

    More cleanups

--- dstk/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-setup(name='dstk',
-      version='0.35',
-      py_modules=['dstk'],
-      author='Pete Warden',
-      author_email='pete@petewarden.com',
-      url='http://www.datasciencetoolkit.org/'
-      )
