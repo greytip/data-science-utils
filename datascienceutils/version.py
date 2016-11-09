ad57426a45b8327720670860e97bb50fb2b6c858
Anand Jeyahar <anand.jeyahar@gmail.com>
2016-11-09 17:57:00 +0530

    Minorversion cahnge commit

--- datascienceutils/version.py
+++ datascienceutils/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM SCIPY SETUP.PY
-short_version = '1.0.29'
-version = '1.0.29'
-full_version = '1.0.29'
-git_revision = '962e349b1991e5334057498214025f7709eb5214'
+short_version = '1.0.30'
+version = '1.0.30'
+full_version = '1.0.30'
+git_revision = '23f04c28c2f404cfb07c31ed0b3b0526a618ff28'
 release = True
 
 if not release:
