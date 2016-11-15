d11ffbd0420eff23549c97a9e71533e455f205d4
anand <anand.jeyahar@gmail.com>
2016-11-15 21:59:23 +0530

    version update..for pypi

--- datascienceutils/version.py
+++ datascienceutils/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM SCIPY SETUP.PY
-short_version = '1.0.31'
-version = '1.0.31'
-full_version = '1.0.31'
-git_revision = '4f9e8fef00f0f9f950b39d6374c96032ca05e290'
+short_version = '1.0.32'
+version = '1.0.32'
+full_version = '1.0.32'
+git_revision = '8a5a3460c01566a40646fc8f5f9d74a4e94f5728'
 release = True
 
 if not release:
