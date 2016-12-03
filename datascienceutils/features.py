b8853011d74cfb2ab9a6cedc6485ffad97843001
anand <anand.jeyahar@gmail.com>
2016-12-03 23:13:48 +0530

    um

--- datascienceutils/features.py
+++ datascienceutils/features.py
@@ -1,5 +1,5 @@
 from sklearn.feature_selection import VarianceThreshold
-
+#TODO: https://www.analyticsvidhya.com/blog/2016/12/introduction-to-feature-selection-methods-with-an-example-or-how-to-select-the-right-variables/?utm_source=feedburner&utm_medium=email&utm_campaign=Feed%3A+AnalyticsVidhya+%28Analytics+Vidhya%29
 #TODO: filter of categorical features by how much of the dataset they divide the records into.
 #TODO: filter of numerical features by variance of the features.
 
